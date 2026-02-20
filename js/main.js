8e90351cd4c0ab98e1ab0c972f8b70b1754c61ab
Wdream <2654887279@qq.com>
2026-02-20 22:37:58 +0800

    更新 网页样式

--- js/main.js
+++ js/main.js
@@ -30,4 +30,60 @@ $(document).ready(function() {
     // 导航按钮被点击
     // this.style.backgroundColor = '#fff'; 设置颜色后会自动消失
   });  
+
+  // Panel Toggle logic
+  $('#panel-toggle').click(function() {
+    if ($('.panel-cover').hasClass('panel-cover--narrow')) {
+        $('.panel-cover').removeClass('panel-cover--narrow');
+        $('.content-wrapper').removeClass('content-wrapper--expanded');
+        $(this).find('i').removeClass('fa-chevron-right').addClass('fa-chevron-left');
+    } else {
+        $('.panel-cover').addClass('panel-cover--narrow');
+        $('.content-wrapper').addClass('content-wrapper--expanded');
+        $(this).find('i').removeClass('fa-chevron-left').addClass('fa-chevron-right');
+    }
+  });
+
+  // Check if we should start narrow
+  var isPostOrBlog = (window.location.pathname != "/" && window.location.pathname != "/index.html") || window.location.hash == "#blog";
+  
+  if (isPostOrBlog) {
+    $('.panel-cover').addClass('panel-cover--narrow');
+    $('.content-wrapper').addClass('content-wrapper--expanded');
+    $('#panel-toggle i').removeClass('fa-chevron-left').addClass('fa-chevron-right');
+  }
+
+  // TOC Toggle logic
+  $('#toc-toggle').click(function() {
+    $('#post-toc').toggleClass('toc-collapsed');
+    $(this).find('.toggle-icon').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+  });
+
+  // TOC Section Folding logic: Fold children by default
+  $('.toc-content > ul > li:has(ul)').each(function() {
+    var $li = $(this);
+    var $link = $li.children('a');
+    
+    // Add fold icon if not exists
+    if ($link.find('.section-toggle').length === 0) {
+        $link.prepend('<i class="fa fa-caret-right section-toggle" style="margin-right:8px; cursor:pointer;"></i>');
+    }
+    
+    $li.addClass('toc-section-folded');
+  });
+
+  // Handle clicking the fold icon
+  $(document).on('click', '.section-toggle', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var $icon = $(this);
+    var $li = $icon.closest('li');
+    if ($li.hasClass('toc-section-folded')) {
+        $li.removeClass('toc-section-folded');
+        $icon.removeClass('fa-caret-right').addClass('fa-caret-down');
+    } else {
+        $li.addClass('toc-section-folded');
+        $icon.removeClass('fa-caret-down').addClass('fa-caret-right');
+    }
+  });
 });
